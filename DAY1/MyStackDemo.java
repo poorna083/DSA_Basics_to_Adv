83357aebfc11dce1183205d12856bbaf263d8541
poorna083 <dpoorna083@gmail.com>
2025-09-09 23:02:44 +0530

    stack

--- DAY1/MyStackDemo.java
+++ DAY1/MyStackDemo.java
@@ -4,8 +4,12 @@ public class MyStackDemo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Stack<String> stack = new Stack<>();
-        // System.out.println(stack.empty());
-        
+        System.out.println(stack.empty());
+        for (int i = 0; i < 5; i++) {
+            stack.add(sc.nextLine());
+        }
+        System.out.println(stack.empty());
+        System.out.println(stack);
         sc.close();
     }
 }
