abb96c293f192a8b7210ad49ecedeb4c5dde58e8
poorna083 <dpoorna083@gmail.com>
2025-09-09 23:00:17 +0530

    Stack

---
+++ DAY1/MyStackDemo.java
@@ -0,0 +1,11 @@
+package DAY1;
+import java.util.*;
+public class MyStackDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Stack<String> stack = new Stack<>();
+        // System.out.println(stack.empty());
+        
+        sc.close();
+    }
+}
