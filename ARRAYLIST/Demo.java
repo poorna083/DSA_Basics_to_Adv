0430703ec3d5d8419e632d766810bd48637f9efb
poorna083 <dpoorna083@gmail.com>
2025-10-11 18:18:56 +0530

    Demo

--- ARRAYLIST/Demo.java
+++ ARRAYLIST/Demo.java
@@ -1,5 +1,21 @@
 package ARRAYLIST;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Demo {
-    
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        for(int i=0;i<size;i++){
+            list.add(new ArrayList<>());
+        }
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                list.get(i).add(sc.nextInt());
+            }
+        }
+        System.out.println(list);
+    }
 }
