f6963a1ba8cc17d347b39c7ec65156c438a840da
poorna083 <dpoorna083@gmail.com>
2025-10-13 18:08:32 +0530

    Demo

--- STRINGSANDSTRINGBUILDERS/Demo.java
+++ STRINGSANDSTRINGBUILDERS/Demo.java
@@ -1,7 +1,12 @@
 package STRINGSANDSTRINGBUILDERS;
 
+import java.util.Scanner;
+
 public class Demo {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        StringBuilder i = new StringBuilder(b)
     }
 }
