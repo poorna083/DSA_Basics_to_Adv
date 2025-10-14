2ba5855ef5be990e3433e3f75d52af7cb6bff397
poorna083 <dpoorna083@gmail.com>
2025-10-14 18:46:33 +0530

    Demo

---
+++ LINEARSEARCH/Demo.java
@@ -0,0 +1,28 @@
+package LINEARSEARCH;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+class Linkedlist{
+    public int isLinkedlist(ArrayList<Integer> list, int target) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+public class Demo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>(); 
+        System.out.println("Enter The size:");
+        int size =sc.nextInt();
+        for(int i=0;i<size;i++){
+            list.add(sc.nextInt());
+        }
+        int target = sc.nextInt();
+        Linkedlist i = new Linkedlist();
+        System.out.println(i.isLinkedlist(list, target));
+    }
+}
